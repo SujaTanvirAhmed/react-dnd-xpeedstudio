4b36f969f78b84d4feb391f8bd7423a01ed2dca1
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-08-11 15:01:31 +0600

    first commit

---
+++ src/Component.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useState } from "react";
+import { useDrag } from "react-dnd";
+import { COMPONENT } from "./constants";
+
+const style = {
+  border: "1px dashed black",
+  padding: "0.5rem 1rem",
+  backgroundColor: "white",
+  cursor: "move"
+};
+
+const Component = ({ data, components, path }) => {
+  const ref = useRef(null);
+
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: COMPONENT, id: data.id, path },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
+  const opacity = isDragging ? 0 : 1;
+  drag(ref);
+
+  const component = components[data.id];
+
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <div
+        ref={ref}
+        style={{ ...style, opacity }}
+        className="component draggable"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        <div>{data.id}</div>
+        <div>{component.content}</div>
+      </div>
+
+      {/* Modal starts */}
+      {
+        showModal &&
+        <div className="modal open" id="modal-one">
+          <div
+            className="modal-bg modal-exit"
+            onClick={() => { setShowModal(false) }}
+          ></div>
+          <div className="modal-container">
+            <h2>ID: {data.id}</h2>
+            <button
+              className="modal-close modal-exit"
+              onClick={() => { setShowModal(false) }}
+            >X</button>
+          </div>
+        </div>
+      }
+      {/* Modal ends */}
+
+    </>
+  );
+
+};
+
+export default Component;
